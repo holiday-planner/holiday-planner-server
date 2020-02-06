16ba6078b80d3254425e1c3e846c6b80a21ad445
nafies1 <ghuroba.muslim@gmail.com>
2020-02-07 04:58:28 +0700

    add home endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const routerHolidays = require('./holidays')
 const holidayController = require('../controllers/holiday');
 const auth = require('./auth')
 
+router.get('/', (req, res) => {
+  res.status(200).json('Welcome to Holiday Planner API')
+})
 router.use('/holidays', routerHolidays)
 router.get('/weather', holidayController.getWeather)
 router.use('/auth', auth)
