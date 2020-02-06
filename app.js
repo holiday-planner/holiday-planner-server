e55594c928126eeb09ddfd614316d8c7d2574f22
nafies1 <ghuroba.muslim@gmail.com>
2020-02-06 16:22:09 +0700

    create boiler-plate

---
+++ app.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const cors = require ('cors')
+// const routes = require('./routes')
+// const errorHandler = require('./middlewares/errorHandler')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+// app.use('/', routes)
+
+// app.use(errorHandler)
+app.get('/', (req, res) => {
+  res.send(process.env.COBA)
+})
+
+console.log(process.env.COBA)
+
+
+app.listen(process.env.PORT, () => {
+  console.log('Running on port', process.env.PORT)
+})
