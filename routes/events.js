c1e944548b1f0fddcfc4cab6a244b567612a86dd
Andara <andarasophan@gmail.com>
2020-02-07 14:52:15 +0700

    fix some errors

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Controller = require('../controllers/events')
 
-router.get('/', Controller.getEvents)
+router.post('/', Controller.getEvents)
 
 module.exports = router;
