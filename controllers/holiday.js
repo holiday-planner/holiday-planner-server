d56ad8ecf851889f37b00ad4be6cf0205788729a
orionslg <orion@DESKTOP-MPHII07.localdomain>
2020-02-07 09:36:52 +0700

    indentation

--- controllers/holiday.js
+++ controllers/holiday.js
@@ -12,12 +12,12 @@ class Controller {
                 next('Not found');
             } else {
                 axios.get(`https://api.darksky.net/forecast/1a39dde7473f0ada56cd3fcef07cd1c2/${coordinate.lat},${coordinate.lng},${date}/`)
-                .then( response => {
-                    res.status(response.status).json(response.data.daily.data[0].summary)
-                })
-                .catch( err => {
-                    next(err);
-                })
+                    .then( response => {
+                        res.status(response.status).json(response.data.daily.data[0].summary)
+                    })
+                    .catch( err => {
+                        next(err);
+                    })
             }
         })
     }
